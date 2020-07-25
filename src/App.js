1e03511a1b20c74dbd608cee5db07740242f2c32
Jumaydev <osipovm33rus@gmail.com>
2020-07-25 11:04:36 +0300

    TabContent added and tested

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import React from 'react';
 import './App.css';
 import TabHead from './components/Tab/TabHead'
+import TabContent from './components/Tab/TabContent'
 
 function App() {
+  let records = [
+    {
+      id: 101,
+      firstName: 'Sue',
+      lastName: 'Corson',
+      email: 'DWhalley@in.gov',
+      phone: '(612)211-6296',
+      address: {
+        streetAddress: '9792 Mattis Ct',
+        city: 'Waukesha',
+        state: 'WI',
+        zip: '22178'
+      },
+      description: 'et lacus magna dolor...',
+    },
+    {
+      id: 102,
+      firstName: 'Sue',
+      lastName: 'Corson',
+      email: 'DWhalley@in.gov',
+      phone: '(612)211-6296',
+      address: {
+        streetAddress: '9792 Mattis Ct',
+        city: 'Waukesha',
+        state: 'WI',
+        zip: '22178'
+      },
+      description: 'et lacus magna dolor...',
+    },
+    {
+      id: 103,
+      firstName: 'Sue',
+      lastName: 'Corson',
+      email: 'DWhalley@in.gov',
+      phone: '(612)211-6296',
+      address: {
+        streetAddress: '9792 Mattis Ct',
+        city: 'Waukesha',
+        state: 'WI',
+        zip: '22178'
+      },
+      description: 'et lacus magna dolor...',
+    }
+  ]
+
   return (
     <div className="App">
       <TabHead />
+      <TabContent records={records}/>
     </div>
   );
 }
---
+++ src/components/Tab/TabContent.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+export default function TabContent(props) {
+    let clickHandle = () => {
+        console.log('click on body')
+    }
+
+
+    let records = props.records.map((record, index) => {
+        const address = record.address;
+        const viewAddress = `${address.streetAddress},\n${address.city} ${address.zip}, ${address.state}`;
+        return (
+            <tr key={index}>
+                <td className='id' name='id'>{record.id}</td>
+                <td className='first-name' name='first-name'>{record.firstName}</td>
+                <td className='last-name' name='last-name'>{record.lastName}</td>
+                <td className='email' name='email'>{record.email}</td>
+                <td className='phone' name='phone'>{record.phone}</td>
+                <td className='address' name='address'>{viewAddress}</td>
+                <td className='description' name='description' title={record.description}>{record.description}</td>
+            </tr>
+        )
+    });
+
+    return (
+        <tbody onClick={clickHandle}>
+            {records}
+        </tbody>
+    )
+}
